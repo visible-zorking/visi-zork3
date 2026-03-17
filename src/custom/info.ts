647edd6567da38990b91931c1ae14fbd2a1e3fc0
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-16 21:37:17 -0400

    Shift info.

---
+++ src/custom/info.ts
@@ -0,0 +1,22 @@
+
+/* Return the initial sourceloc to display. */
+export function sourceloc_start() : string
+{
+    return 'L:232:1:246:0';  // 'verbs.zil', lines 232-246
+}
+
+// Presentation order. Filenames must match game-info!
+export const sourcefile_presentation_list: string[] = [
+    'zork3.zil',
+    'actions.zil',
+    'shadow.zil',
+    'dungeon.zil',
+    'tm.zil',
+    'main.zil',
+    'demons.zil',
+    'macros.zil',
+    'parser.zil',
+    'syntax.zil',
+    'verbs.zil',
+    'clock.zil',
+];
